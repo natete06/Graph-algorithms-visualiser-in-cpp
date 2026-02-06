2945c09ec9f93f14efc12a12a2ef81ed26f57251
Nahom Tesfaye <ntesfaye@constructor.university>
2026-02-06 03:52:54 +0100

    bfs implementation

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <queue>
+#include <unordered_set>
+#include <algorithm>
 
 class Graph{
+    
+    private:
+    // Node : neighbours using container nesting
+        std::map<int, std::vector<int>> map_graph;
+
     public:
-        Graph() {}
+        Graph() {
+
+        }
+
         void addNode(int Node) {
             map_graph[Node] = {}; 
         }
-        // vector to store neighbors of each Node
+        // update vector which stores neighbors of each Node
         void addEdge(int Vert1, int Vert2) {
-            // creates an equivalence of nodes
+            // Add the connection
             map_graph[Vert1].push_back(Vert2);
+            
+            // LEXICOGRAPHIC ORDER 
+            std::sort(map_graph[Vert1].begin(), map_graph[Vert1].end());
+
+            // same here
             map_graph[Vert2].push_back(Vert1);
+            std::sort(map_graph[Vert2].begin(), map_graph[Vert2].end());
         }
         void printGraph() {
             // loops through each key(node) and prints the values from the vector(neighbours) using an iterator object
@@ -34,17 +51,70 @@ class Graph{
             return map_graph[Node];
         }
 
-    private:
-        std::map<int, std::vector<int>> map_graph;
 
 };
 
+void BFS (Graph *g, int begin) {
+    std::queue<int> bfs_Queue;      
+    std::unordered_set<int> visited = {};
+
+    visited.insert(begin);
+    bfs_Queue.push(begin);
+
+    while (!bfs_Queue.empty()) {
+        //store before popping
+        int current = bfs_Queue.front();
+        std::cout<< "Visiting: " <<current<<std::endl;
+        //dequeue current node
+        bfs_Queue.pop();
+        // neighbors stored for iteration
+
+        const std::vector<int>& neighbors = g->GetNbr(current);
+        for (int i = 0; i < neighbors.size(); i++) {
+            if (visited.count(neighbors[i]) == 0) {
+                bfs_Queue.push(neighbors[i]);
+                visited.insert(neighbors[i]);
+            }
+        }
+    }
+
+
+}
+
 int main() {
-    Graph g;
-    g.addNode(1);
-    g.addNode(2);
-    g.addNode(3);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.printGraph();
+    
+
+    std::cout << "\n\n==========================" << std::endl;
+    std::cout << "   TESTING GRAPH G2" << std::endl;
+    std::cout << "==========================\n" << std::endl;
+
+    Graph g2;
+
+    // 1. Add nodes (using 0-9 to keep it simple but larger)
+    for (int i = 0; i < 10; i++) {
+        g2.addNode(i);
+    }
+
+    // 2. Add "Random" Edges to create a complex web
+    // This includes cycles (loops) which will crash your program
+    // if the 'visited' set isn't working properly.
+    g2.addEdge(0, 1);
+    g2.addEdge(0, 4);
+    g2.addEdge(1, 2);
+    g2.addEdge(1, 3);
+    g2.addEdge(1, 4); // Creates a triangle cycle (0-1-4)
+    g2.addEdge(2, 5);
+    g2.addEdge(3, 6);
+    g2.addEdge(4, 5); // Creates a cycle (1-2-5-4)
+    g2.addEdge(4, 7);
+    g2.addEdge(5, 8);
+    g2.addEdge(6, 7); // Creates a path convergence at 7
+    g2.addEdge(7, 8); // Creates a cycle (4-5-8-7)
+    g2.addEdge(8, 9); // The 'tail' of the graph
+
+    std::cout << "--- Structure of G2 ---" << std::endl;
+    g2.printGraph();
+
+    std::cout << "\n--- Running BFS on G2 (Start Node: 0) ---" << std::endl;
+    BFS(&g2, 0);
 }
---
+++ Graph/scrap.cpp
@@ -0,0 +1,7 @@
+Graph g;
+    g.addNode(1);
+    g.addNode(2);
+    g.addNode(3);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.printGraph();
